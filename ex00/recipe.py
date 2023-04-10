d155bb197d50557d18486be8f1409d74c06bc1d6
Piriou Enzo <penzo@student.42.fr>
2023-04-10 18:09:48 +0200

    wip

--- ex00/recipe.py
+++ ex00/recipe.py
@@ -1,5 +1,10 @@
-class Recipe:
-    def __init__(self, name: str, cooking_lvl: int, cooking_time: int, ingredients: list, description: str, recipe_type: str):
+recipe_types = ["starter", "lunch", "dessert"]
+
+
+class Recipe():
+    def __init__(self, name: str, cooking_lvl: int,
+                 cooking_time: int, ingredients: list,
+                 description: str, recipe_type: str):
         # check name
         if name is None or len(name) == 0:
             raise ValueError('no name given.')
@@ -23,10 +28,17 @@ class Recipe:
             raise ValueError('ingredients is not a list.')
         if len(ingredients) == 0:
             raise ValueError('ingredients is an empty list.')
-        print(ingredients)
-        # NOT WORKING
-        if [type(ingre) is not str for ingre in ingredients]:
+        if any(type(ingre) is not str for ingre in ingredients):
             raise ValueError('ingredients have non string ingredients.')
+        if any(ingre.strip() == '' for ingre in ingredients):
+            raise ValueError('ingredients have empty ingredient.')
+        # TODO: strip ingredients
+        # check recipe_type
+        if type(recipe_type) is not str:
+            raise ValueError('recipe_type is not str.')
+        if recipe_type not in recipe_types:
+            # TODO: raise error !
+            print("NOOOP!!!!")
 
     def __str__(self):
         """Return the string to print with the recipe info"""
--- ex00/test.py
+++ ex00/test.py
@@ -1,9 +1,9 @@
-from book import Book 
+from book import Book
 from recipe import Recipe
 
 book = Book('ntoheu')
 
 # print(book)
 
-recipe = Recipe('dru', 1, 0, ['oeu', 'hnotetuh'], 'desc', 'type')
+recipe = Recipe('dru', 1, 0, ['oeu', 'ntehuint'], 'desc', 'lunchnotheu')
 print(recipe.cooking_lvl)
